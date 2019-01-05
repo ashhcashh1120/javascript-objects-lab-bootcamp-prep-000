b6e39f6c618b3330abf9f865b79f52b90c598261
Ashley Achibald <aarchibald20@gmail.com>
2019-01-05 09:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 var recipes ={
 
-}
-function updateObjectWithKeyAndValue(object, key, value){
-  const newRecipes = Object.assign({}, object, key, value )
-}
 // update the object with key an value & return orginal key value
 //pairs and then the new key value.
 function  updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object ,{[key]: value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key,
+ value){
+   const newRecipes = 
+ }
